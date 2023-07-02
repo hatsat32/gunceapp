1a90c3098cdbb6beaaff4140f2f6b6c93eb223ee
Süleyman ERGEN <hatsat32@protonmail.com>
2023-07-02 15:08:50 +0300

    update requirements and conftest

--- gunceserver/tests/conftest.py
+++ gunceserver/tests/conftest.py
@@ -1,24 +1,8 @@
 import pytest
 from starlette.testclient import TestClient
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
 
 from main import app
-from core.deps import get_db
-from core.db import Base
-
-SQLALCHEMY_DATABASE_URL = "postgresql://gunce:gunce@localhost/guncetest"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-def override_get_db():
-    try:
-        db = TestingSessionLocal()
-        yield db
-    finally:
-        db.close()
+from core.db import Base, engine
 
 
 @pytest.fixture(scope="session")
@@ -31,6 +15,4 @@ def db_session():
 
 @pytest.fixture(scope="session")
 def test_app(db_session) -> TestClient:
-    app.dependency_overrides[get_db] = override_get_db
-
     yield TestClient(app)  # testing happens here
